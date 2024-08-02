ab4607c9864568d934437c5e2b41a088d96e1e0e
itavero <203398+itavero@users.noreply.github.com>
2024-08-02 16:03:44 +0000

    Docs generated based on zigbee-herdsman-converters v19.72.0

--- src/docgen/versions.ts
+++ src/docgen/versions.ts
@@ -1,4 +1,4 @@
 // GENERATED FILE: DO NOT EDIT MANUALLY!
-const version_zigbee2mqtt = '1.39.0';
-const version_herdsman_converters = '19.62.0';
+const version_zigbee2mqtt = '1.39.1';
+const version_herdsman_converters = '19.72.0';
 export { version_zigbee2mqtt, version_herdsman_converters };

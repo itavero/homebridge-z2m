32ac9ef38d577999d5fe92c74caca4fda0b42e52
burmistrzak <61958704+burmistrzak@users.noreply.github.com>
2024-09-10 17:29:50 +0200

    Add `EXP_SKIP_BRIGHTNESS_UPDATE`

--- src/platformAccessory.ts
+++ src/platformAccessory.ts
@@ -16,7 +16,7 @@ import {
 import { BaseDeviceConfiguration, isDeviceConfiguration } from './configModels';
 import { QoS } from 'mqtt-packet';
 import { sanitizeAndFilterExposesEntries } from './helpers';
-import { EXP_AVAILABILITY } from './experimental';
+import { EXP_AVAILABILITY, EXP_SKIP_BRIGHTNESS_UPDATE } from './experimental';
 
 export class Zigbee2mqttAccessory implements BasicAccessory {
   private readonly updateTimer: ExtendedTimer;
@@ -301,8 +301,8 @@ export class Zigbee2mqttAccessory implements BasicAccessory {
       }, []);
     if (keys.length > 0) {
       // Do not ask Zigbee2MQTT for brightness, otherwise the cached brightness level gets lost when a given bulb's state is OFF.
-      if (this.platform.isExperimentalFeatureEnabled('SKIP_BRIGHTNESS_UPDATE') && keys.includes('brightness')) {
-        keys = keys.filter((key) => key !== String('brightness'));
+      if (this.platform.isExperimentalFeatureEnabled(EXP_SKIP_BRIGHTNESS_UPDATE) && keys.includes('brightness')) {
+        keys = keys.filter((key) => key !== 'brightness');
       }
       this.queueKeyForGetAction(keys);
     }

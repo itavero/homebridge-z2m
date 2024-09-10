8798871c78c8971047e217c478211c4bf29810c5
burmistrzak <61958704+burmistrzak@users.noreply.github.com>
2024-09-10 17:08:50 +0200

    Implement `SKIP_BRIGHTNESS_UPDATE` for `queueAllKeysForGet`

--- src/platformAccessory.ts
+++ src/platformAccessory.ts
@@ -294,12 +294,16 @@ export class Zigbee2mqttAccessory implements BasicAccessory {
   }
 
   private queueAllKeysForGet(): void {
-    const keys = [...this.serviceHandlers.values()]
+    let keys = [...this.serviceHandlers.values()]
       .map((h) => h.getableKeys)
       .reduce((a, b) => {
         return a.concat(b);
       }, []);
     if (keys.length > 0) {
+      // Do not ask Zigbee2MQTT for brightness, otherwise the cached brightness level gets lost when a given bulb's state is OFF.
+      if (this.platform.isExperimentalFeatureEnabled('SKIP_BRIGHTNESS_UPDATE') && keys.includes('brightness')) {
+        keys = keys.filter((key) => key !== String('brightness'));
+      }
       this.queueKeyForGetAction(keys);
     }
   }
